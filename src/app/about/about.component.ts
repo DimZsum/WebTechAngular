eaf5e56a0fabd010a6cb8d6f069e12af6afa8a7b
Hoan To <hoan.to91@gmail.com>
2018-05-13 21:13:13 +0200

    finished

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../data.service';
+import { HomeComponent } from '../home/home.component';
+
 
 @Component({
   selector: 'app-about',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  goals: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private _data: DataService) {
+    this.route.params.subscribe(res => console.log(res));
+    //weiter machen hier
+    // for (let i = 0; this.goals.lenght; i++) {
+    //   window.alert(i);
+    // }
+  }
 
   ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res );
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,31 @@
 import {Component, OnInit} from '@angular/core';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+import { DataService} from "../data.service";
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('goals', [
+      transition('* => *', [
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: true}),
+
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-out', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
+          ]))]), {optional: true})
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
@@ -12,23 +34,33 @@ export class HomeComponent implements OnInit {
   goalText = 'My first life goal';
   goals = [];
 
-  constructor() {
+  constructor(private _data: DataService) {
   }
 
   ngOnInit() {
     this.itemCount = this.goals.length;
+    this._data.goal.subscribe(res => this.goals = res);
+    // this._data.changeGoal(this.goals);
   }
 
   addItem() {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
+
   }
   increaseItemCount(){
     this.itemCount++;
   }
   changeGoalText(){
     this.goalText = 'This is a test';
+  }
+  removeItem(i){
+    this.goals.splice(i,1);
+    this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
+
   }
 
 }
